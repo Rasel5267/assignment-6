3108e51fc4094d27bf2a3b41d491a70ecd239586
Rasel5267 <mahadiul09@gmail.com>
2023-07-28 17:32:16 +0600

    [done] add category page and fetch data

---
+++ src/pages/category/[category].tsx
@@ -0,0 +1,40 @@
+import ProductCard from "@/components/UI/ProductCard";
+import Layout from "@/components/layout/Layout";
+import { IProduct } from "@/types/globalTypes";
+import React, { ReactElement } from "react";
+
+interface Props {
+	products: IProduct[];
+}
+
+const Category = ({ products }: Props) => {
+	return (
+		<div className="my-12 px-5 mx-auto">
+			<div className="grid grid-auto-fit-lg gap-4">
+				{products.map((product: IProduct) => (
+					<ProductCard product={product} key={product._id} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+Category.getLayout = function getLayout(page: ReactElement) {
+	return <Layout>{page}</Layout>;
+};
+
+export default Category;
+
+export const getServerSideProps = async (context: { params: any }) => {
+	const { params } = context;
+	const res = await fetch(
+		`https://pc-house.vercel.app/products/category/${params.category}`
+	);
+	const data = await res.json();
+
+	return {
+		props: {
+			products: data,
+		},
+	};
+};
